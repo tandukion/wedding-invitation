e3af16f94056f54ca4b597d4f956a58af26ce87e
Dwindra <tandukion@hotmail.com>
2022-08-08 08:03:04 +0900

    Add page starter for guest

--- js/script.js
+++ js/script.js
@@ -4,10 +4,29 @@
 
 (function($) {
   "use strict"; // Start of use strict
+
+  // Get the URL parameters
+  const params = (new URL(document.location)).searchParams;
+
+  // Get the guest name
+  const guestName = params.get('u');
+  if (guestName) {
+    $('#guest-name').text(guestName);
+  }
+  else {
+    // Directly open the main page if no guest name provided
+    $('#page-starter').toggleClass('hidden');
+    $('#main-page').toggleClass('hidden');
+  }
+
+  // Open page button
+  $('#button-open-page').click(function(){
+    $('#page-starter').toggleClass('fixed-page-closed');
+    $('#main-page').toggleClass('hidden');
+  })
   
   // Smooth scrolling using jQuery Easing
   $('a.js-scroll-trigger[href*="#"]').click(function() {
-    console.log('AAAAAAAAa')
     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
       let target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
